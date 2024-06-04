2d880a462f040bca78b6eccedf000fab72714519
daisuke yamamoto <yamamoto.daisuke.1012@gmail.com>
2024-06-04 10:10:59 +0000

    Add

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,16 +1,32 @@
 import { defineBackend } from "@aws-amplify/backend";
+import { auth } from "./auth/resource";
 import { data } from "./data/resource";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import { Construct } from "constructs";
 
-defineBackend({ data });
+const backend = defineBackend({
+  auth,
+  data,
+});
 
-export class CustomVpc extends Construct {
-  public readonly vpc: ec2.Vpc;
+export class CustomDatabase extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
-    this.vpc = new ec2.Vpc(this, "CustomVpc", {
-      ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/16"),
+    const vpc = new ec2.Vpc(this, "ampxVPC", {
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "public",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+      ],
     });
+    const securityGroup = new ec2.SecurityGroup(this, "RDSSecurityGroup", {
+      vpc,
+      allowAllOutbound: true,
+    });
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(3306));
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443));
   }
 }

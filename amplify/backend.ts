0164f8757e7c573bfcf166f3f759b914602b099b
daisuke yamamoto <yamamoto.daisuke.1012@gmail.com>
2024-06-04 05:58:21 +0000

    Add

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,23 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { CustomDatabase } from './custom/customdatabase/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
+});
+
+const customDatabase = new CustomDatabase(
+  backend.createStack('CustomDatabase'),
+  'CustomDatabase',
+);
+
+backend.addOutput({
+  custom: {
+    db: customDatabase,
+  },
 });

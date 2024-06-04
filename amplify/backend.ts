1f06c293a6ffadfca41f47c8b7f814773016a645
daisuke yamamoto <yamamoto.daisuke.1012@gmail.com>
2024-06-04 10:45:14 +0000

    Add

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,55 +2,60 @@ import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource";
 import { data } from "./data/resource";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
-import { Construct } from "constructs";
 import * as rds from "aws-cdk-lib/aws-rds";
 
 const backend = defineBackend({
   auth,
   data,
 });
-export class CustomDatabase extends Construct {
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
-    const vpc = new ec2.Vpc(this, "ampxVPC", {
-      maxAzs: 2,
-      subnetConfiguration: [
-        {
-          cidrMask: 24,
-          name: "public",
-          subnetType: ec2.SubnetType.PUBLIC,
-        },
-      ],
-    });
-    const securityGroup = new ec2.SecurityGroup(this, "RDSSecurityGroup", {
-      vpc,
-      allowAllOutbound: true,
-    });
-    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(3306));
-    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443));
 
-    const rdsInstance = new rds.DatabaseInstance(this, "RDSInstance", {
-      engine: rds.DatabaseInstanceEngine.mysql({
-        version: rds.MysqlEngineVersion.VER_8_0_36,
-      }),
-      credentials: rds.Credentials.fromGeneratedSecret("admin"),
-      instanceType: ec2.InstanceType.of(
-        ec2.InstanceClass.BURSTABLE3,
-        ec2.InstanceSize.SMALL,
-      ),
-      vpc,
-      vpcSubnets: {
-        subnetType: ec2.SubnetType.PUBLIC,
-      },
-      securityGroups: [securityGroup],
-      databaseName: "ampxdb",
-      publiclyAccessible: true,
-    });
+const customResourceStack = backend.createStack("MyCustomResources");
 
-    backend.addOutput({
-      custom: {
-        value: rdsInstance.dbInstanceEndpointAddress,
-      },
-    });
-  }
-}
+const vpc = new ec2.Vpc(customResourceStack, "ampxVPC", {
+  maxAzs: 2,
+  subnetConfiguration: [
+    {
+      cidrMask: 24,
+      name: "public",
+      subnetType: ec2.SubnetType.PUBLIC,
+    },
+  ],
+});
+const securityGroup = new ec2.SecurityGroup(
+  customResourceStack,
+  "RDSSecurityGroup",
+  {
+    vpc,
+    allowAllOutbound: true,
+  },
+);
+securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(3306));
+securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443));
+
+const rdsInstance = new rds.DatabaseInstance(
+  customResourceStack,
+  "RDSInstance",
+  {
+    engine: rds.DatabaseInstanceEngine.mysql({
+      version: rds.MysqlEngineVersion.VER_8_0_36,
+    }),
+    credentials: rds.Credentials.fromGeneratedSecret("admin"),
+    instanceType: ec2.InstanceType.of(
+      ec2.InstanceClass.BURSTABLE3,
+      ec2.InstanceSize.SMALL,
+    ),
+    vpc,
+    vpcSubnets: {
+      subnetType: ec2.SubnetType.PUBLIC,
+    },
+    securityGroups: [securityGroup],
+    databaseName: "ampxdb",
+    publiclyAccessible: true,
+  },
+);
+
+backend.addOutput({
+  custom: {
+    value: rdsInstance.dbInstanceEndpointAddress,
+  },
+});

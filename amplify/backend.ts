ada451549c2601f971cbea50894551133a52704b
daisuke yamamoto <yamamoto.daisuke.1012@gmail.com>
2024-06-06 09:46:54 +0000

    Add

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,6 +3,7 @@ import { auth } from "./auth/resource";
 import { data } from "./data/resource";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as rds from "aws-cdk-lib/aws-rds";
+import * as ecr from "aws-cdk-lib/aws-ecr";
 
 const backend = defineBackend({
   auth,
@@ -11,6 +12,8 @@ const backend = defineBackend({
 
 const customResourceStack = backend.createStack("KBPCustomResources");
 
+// VPC
+// README at: https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ec2-readme.html
 const kbpCustomVpc = new ec2.Vpc(customResourceStack, "kbpAmpxVPC", {
   //  ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
   maxAzs: 2,
@@ -33,6 +36,16 @@ const kbpSecurityGroup = new ec2.SecurityGroup(
 kbpSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(3306));
 kbpSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443));
 
+// ECR
+// README at: https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ecr-readme.html
+const repository = new ecr.Repository(customResourceStack, "kbpRepo", {
+  imageScanOnPush: false,
+  repositoryName: "backend-api-1",
+});
+repository.addLifecycleRule({ tagPatternList: ["v*"], maxImageCount: 10 });
+
+// RDS
+// README at: https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ecr-readme.html
 const kbpRdsInstance = new rds.DatabaseInstance(
   customResourceStack,
   "kbpRDSInstance",
@@ -57,6 +70,6 @@ const kbpRdsInstance = new rds.DatabaseInstance(
 
 backend.addOutput({
   custom: {
-    value: kbpRdsInstance.dbInstanceEndpointAddress,
+    rds: kbpRdsInstance.dbInstanceEndpointAddress,
   },
 });

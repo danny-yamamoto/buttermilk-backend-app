a7d9eb0ab2b769f8e0b5d0f937f1090a96dc6705
daisuke yamamoto <yamamoto.daisuke.1012@gmail.com>
2024-06-13 11:49:47 +0000

    delete

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from "@nestjs/common";
 import { HogeModule } from "./hoge/hoge.module";
-import { TodoModule } from "./todo/todo.module";
 
 @Module({ imports: [HogeModule] })
 export class AppModule {}

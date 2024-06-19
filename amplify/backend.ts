41b89e1d9b9cc35a458ab75daeefec50a58ee285
daisuke yamamoto <yamamoto.daisuke.1012@gmail.com>
2024-06-19 03:43:29 +0000

    Add

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,6 +2,7 @@ import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource";
 import { data } from "./data/resource";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
 import dotenv from "dotenv";
 
 const backend = defineBackend({
@@ -53,3 +54,51 @@ kbpSecurityGroup.addIngressRule(
   ec2.Port.tcp(80),
   "Allow HTTP traffic",
 );
+
+const kbpCluster = new ecs.Cluster(customResourceStack, "kbpCluster", {
+  vpc: kbpCustomVpc,
+});
+
+const taskDefinition = new ecs.FargateTaskDefinition(
+  customResourceStack,
+  "kbpFargateTaskDefinition",
+  {
+    memoryLimitMiB: 512,
+    cpu: 256,
+  },
+);
+
+// ECS
+// README at: https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ecs-readme.html
+taskDefinition.addContainer("fargate-app", {
+  image: ecs.ContainerImage.fromAsset("./"),
+  portMappings: [
+    {
+      containerPort: 80, // コンテナ内部のポート
+      protocol: ecs.Protocol.TCP,
+    },
+  ],
+});
+
+const kbpFargateServiceSecurityGroup = new ec2.SecurityGroup(
+  customResourceStack,
+  "kbpFargateServiceSecurityGroup",
+  {
+    vpc: kbpCustomVpc,
+    description: "Allow access to Fargate service",
+    allowAllOutbound: true,
+  },
+);
+
+kbpFargateServiceSecurityGroup.addIngressRule(
+  ec2.Peer.anyIpv4(),
+  ec2.Port.tcp(80),
+  "Allow HTTP traffic",
+);
+
+new ecs.FargateService(customResourceStack, "kbpFargateService", {
+  cluster: kbpCluster,
+  taskDefinition: taskDefinition,
+  assignPublicIp: true,
+  securityGroups: [kbpFargateServiceSecurityGroup],
+});
